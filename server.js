fc3ff2666624a8df7370272bd0a144f067aab542
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-05-05 13:17:11 +0200

    test middleware

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const PORT = 3000;
 // Middleware för att hantera JSON-begärningar
 app.use(express.json());
 
+// Test middleware
+app.use((req, res, next) => {
+  console.log('middleware hej');
+  console.log(req.url);
+  console.log(req.method);
+  next();
+});
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello welcome' });
 });
